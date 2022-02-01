8699c4aa39cee6ffef431a2fa25b480da2ed751f
JohnRudolph1 <johnrudolph963@gmail.com>
2022-01-31 20:00:16 -0600

    first commit

---
+++ src/template.js
@@ -0,0 +1,11 @@
+export const jobTemplate = (job, currency) => `
+<div class="card">
+  <div class="card-body">
+  <h4 class="card-title">${job.title}</h4>
+  <h5>${job.location.display_name}</h5>
+  <p class="card-text">${job.description}</p>
+  <a href="${job.redirect_url}">View Job</a><i class="far fa-heart-circle"></i>
+  
+  </div>
+</div>
+`;
